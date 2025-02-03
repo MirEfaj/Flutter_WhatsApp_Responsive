5566232b0b963c2e0447db019a2efc79ff13c60f
mdefa <mdefaj50@gmail.com>
2025-02-03 22:50:31 +0600

    one

--- lib/Widgets/info.dart
+++ lib/Widgets/info.dart
@@ -5,7 +5,7 @@ const info = [
     'time': '3:53 pm',
     'profilePic':
     'https://upload.wikimedia.org/wikipedia/commons/8/85/Elon_Musk_Royal_Society_%28crop1%29.jpg',
-     
+
   },
   {
     'name': 'John Doe',
